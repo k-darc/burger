d0683efd33f2979d64cef6ca7593e0594406c76c
Kevin Darcy <neoglitch822@yahoo.com>
2019-11-05 21:27:45 -0500

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("hamburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon_cheeseburger", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES ("veggie_burger", 1);
